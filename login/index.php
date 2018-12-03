6351a1c42e8ff6dc55a3a04b816c6d5195f4f33d
Anaeb <45439696+Anaeb@users.noreply.github.com>
2018-12-03 14:48:40 -0600

    cambio registro365

--- login/index.php
+++ login/index.php
@@ -17,7 +17,7 @@ if(isset($_SESSION['session']))
 <body>
 	
 	<div id="registrar">
-          	<a href="./php/registro.php"</a>registro 536</a>
+          	<a href="./php/registro.php"</a>registro365</a>
     </div> <!-- fin opcion-->
     
 	<div id="envoltura">
