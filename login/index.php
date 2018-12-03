036a47ba934da03b73e89c197341e5b213337229
santiagogabriel <45430974+santiagogabriel@users.noreply.github.com>
2018-12-03 16:33:20 -0600

    cambio de 536

--- login/index.php
+++ login/index.php
@@ -49,7 +49,7 @@ if(isset($_SESSION['session']))
 				</form>
 			</div><!--fin cuerpo-->
 	
-			<div id="pie">informacion</div>
+			<div id="pie">informacion de 536</div>
 		</div><!-- fin contenedor -->
 
 	</div><!--fin envoltura-->
