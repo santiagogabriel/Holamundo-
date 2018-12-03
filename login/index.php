c95ab4f15935929f54320f55cf2da670b6b5c0bf
Anaeb <45439696+Anaeb@users.noreply.github.com>
2018-12-03 15:00:40 -0600

    cambio registro ana

--- login/index.php
+++ login/index.php
@@ -17,7 +17,7 @@ if(isset($_SESSION['session']))
 <body>
 	
 	<div id="registrar">
-          	<a href="./php/registro.php"</a>registro365</a>
+          	<a href="./php/registro.php"</a>registro ana</a>
     </div> <!-- fin opcion-->
     
 	<div id="envoltura">
