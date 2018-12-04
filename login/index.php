218ad94f05a4293d87d9af02886692c97f96a936
PeraltaVillalobos <45442095+PeraltaVillalobos@users.noreply.github.com>
2018-12-03 23:33:33 -0600

    Update index.php

--- login/index.php
+++ login/index.php
@@ -49,7 +49,7 @@ if(isset($_SESSION['session']))
 				</form>
 			</div><!--fin cuerpo-->
 	
-			<div id="pie">Datos de 5semestre</div>
+			<div id="pie">Datos del cobao</div>
 		</div><!-- fin contenedor -->
 
 	</div><!--fin envoltura-->
